5f17ca946391259c9a032da95aac516d33402e9e
Suns <songyangmri@gmail.com>
2024-07-17 15:00:55 +0800

    Initial Template Updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MedIA'
+# copyright = '2021, Graziella'
+author = 'Suns'
 
 release = '0.1'
 version = '0.1.0'
